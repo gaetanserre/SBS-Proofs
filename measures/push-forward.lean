35e6efc84711abd3b23d061578785ff57481c2c6
Gaëtan Serré <gaetan.serre@ens-paris-saclay.fr>
2023-07-11 19:56:46 +0200

    push-forward measure - first steps

---
+++ measures/push-forward.lean
@@ -0,0 +1,7 @@
+import Mathlib.MeasureTheory.Measure.Lebesgue.Basic
+
+namespace MeasureTheory
+
+structure Pushforward_Measure (α : Type _) (β : Type _) [MeasurableSpace α] [MeasurableSpace β] where
+μ : Measure α
+f : Set α -> Set β

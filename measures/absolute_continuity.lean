5d5269c408c9fd8d94ed89530d2e4cd91743c037
Gaëtan Serré <gaetan.serre@ens-paris-saclay.fr>
2023-07-12 18:01:30 +0200

    Measures

--- measures/absolute_continuity.lean
+++ measures/absolute_continuity.lean
@@ -2,7 +2,7 @@ import Mathlib.MeasureTheory.Measure.Lebesgue.Basic
 
 namespace MeasureTheory
 
-def absolutely_continuous {α : Type _} {_m : MeasurableSpace α} (μ ν : Measure α) : Prop := ∀ (s : Set α), ν s = 0 → μ s = 0
+def absolutely_continuous {α : Type _} {_m : MeasurableSpace α} (μ ν : Measure α) := ∀ (s : Set α), ν s = 0 → μ s = 0
 
 lemma absolutely_continuous_trans {α : Type _} {_m : MeasurableSpace α} (μ₁ μ₂ μ₃ : Measure α) (h1: absolutely_continuous μ₁ μ₂) (h2: absolutely_continuous μ₂ μ₃) : absolutely_continuous μ₁ μ₃ := by
   intros s hsigma
--- measures/push-forward.lean
+++ measures/push-forward.lean
@@ -1,6 +1,17 @@
 import Mathlib.MeasureTheory.Measure.Lebesgue.Basic
+import Mathlib.Data.Real.Basic
 
 namespace MeasureTheory
+
+
+structure homeomorphism (α : Type _) (β : Type _)
+where
+f : α -> β
+inv_f : β -> α
+
+def is_injective {α : Type _} {β : Type _} (hom : homeomorphism α β) := ∀ (sα : Set α), ∀ (sβ : Set β), ∀ a₁ ∈ sα, ∀ a₂ ∈ sα, a₁ ≠ a₂ -> ∃ b₁ ∈ sβ, ∃ b₂ ∈ sβ, b₁ ≠ b₂ ∧ hom.f a₁ = b₁ ∧ hom.f a₂ = b₂
+
+def is_inversible {α : Type _} {β : Type _} (hom : homeomorphism α β) := ∀ (sα : Set α), ∀ (sβ : Set β), ∀ a ∈ sα, ∃ b ∈ sβ, hom.f a = b <-> hom.inv_f b = a
 
 structure Pushforward_Measure (α : Type _) (β : Type _) [MeasurableSpace α] [MeasurableSpace β] where
 μ : Measure α

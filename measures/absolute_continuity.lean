a18235c1ecb4a9a4226c43bb29839b82adda644a
Gaëtan Serré <gaetan.serre@ens-paris-saclay.fr>
2023-07-12 16:24:27 +0200

    remove duplicate

--- measures/absolute_continuity.lean
+++ measures/absolute_continuity.lean
@@ -4,16 +4,10 @@ namespace MeasureTheory
 
 def absolutely_continuous {α : Type _} {_m : MeasurableSpace α} (μ ν : Measure α) : Prop := ∀ (s : Set α), ν s = 0 → μ s = 0
 
-lemma absolutely_continuous_trans {α : Type _} {_m : MeasurableSpace α} (μ ν σ : Measure α) (h1: absolutely_continuous μ ν) (h2: absolutely_continuous ν σ) : absolutely_continuous μ σ := by
+lemma absolutely_continuous_trans {α : Type _} {_m : MeasurableSpace α} (μ₁ μ₂ μ₃ : Measure α) (h1: absolutely_continuous μ₁ μ₂) (h2: absolutely_continuous μ₂ μ₃) : absolutely_continuous μ₁ μ₃ := by
   intros s hsigma
-  specialize h1 s
-  specialize h2 s
-  exact h1 (h2 hsigma)
+  exact h1 s (h2 s hsigma)
 
 lemma absolutely_continuous_of_self {α : Type _} {_m : MeasurableSpace α} (μ : Measure α) : absolutely_continuous μ μ := by
   intros _s h
   exact h
-
-structure Pushforward_Measure (α : Type _) (β : Type _) [MeasurableSpace α] [MeasurableSpace β] where
-μ : Measure α
-f : Set α -> Set β

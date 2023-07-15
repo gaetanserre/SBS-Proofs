c5a672829662f3d73a413387a0a835fbe23f256b
Gaëtan Serré <gaetan.serre@ens-paris-saclay.fr>
2023-07-15 10:11:19 +0200

    PushForward

--- measures/PushForward.lean
+++ measures/PushForward.lean
@@ -1,8 +1,16 @@
 import Mathlib.Tactic
 import Mathlib.MeasureTheory.Measure.Lebesgue.Basic
+open Classical Topology BigOperators Filter ENNReal NNReal
 
 namespace MeasureTheory
 
-structure Pushforward_Measure (α : Type _) (β : Type _) [MeasurableSpace α] [MeasurableSpace β] where
+structure Pushforward_Measure (α : Type _) (β : Type _) [MeasurableSpace α] [MeasurableSpace β] extends Measure β where
 μ : Measure α
-f : α -> β
+f : Set α -> Set β
+f_inv : Set β -> Set α
+
+instance Pushforward_Measure.instCoeFun {α : Type _} {β : Type _} [MeasurableSpace α] [MeasurableSpace β] : CoeFun (Pushforward_Measure α β) fun _ => Set β → ℝ≥0∞ :=
+  ⟨fun m => m.μ.toOuterMeasure ∘ m.f_inv⟩
+
+variable (α : Type _) (β : Type _) (h1 : MeasurableSpace α) (h2 : MeasurableSpace β) (μ : Pushforward_Measure α β) (s : Set β)
+#check μ s

1fe8a6cb3795349c1a6212f98c5222dd04d99226
Gaëtan Serré <gaetan.serre@ens-paris-saclay.fr>
2023-07-17 17:33:31 +0200

    PushForward Measure

--- GOSVGDProofs/Bijection.lean
+++ GOSVGDProofs/Bijection.lean
@@ -41,12 +41,6 @@ cases h with
 
 def is_reciprocal {α : Type _} {β : Type _} (f : α -> β) (f_inv : β -> α) := (∀ (b : β), f (f_inv b) = b) ∧ (∀ (a : α), f_inv (f a) = a)
 
-lemma reciprocal_of_bij_is_surj {α : Type _} {β : Type _} (f : α -> β) (f_inv : β -> α) (h1 : is_reciprocal f f_inv) (h2 : is_bijective f) : is_surjective f_inv :=
-by
-intro a
-use (f a)
-exact h1.right a
-
 lemma deterministic_function {α : Type _} {β : Type _} (f : α -> β) : ∀ (a₁ a₂ : α), f a₁ ≠ f a₂ -> a₁ ≠ a₂ := by
 intros a₁ a₂ h
 contrapose h
@@ -54,21 +48,27 @@ push_neg at h
 push_neg
 rw [h]
 
-lemma reciprocal_of_bij_is_inj {α : Type _} {β : Type _} (f : α -> β) (f_inv : β -> α) (h1 : is_reciprocal f f_inv) (h2 : is_bijective f) : is_injective f_inv :=
-by
-intros b₁ b₂ difb₁b₂
-have key1 : ∃ (a : α), f a = b₁ := h2.right b₁
-have key2 : ∃ (a : α), f a = b₂ := h2.right b₂
-cases key1 with 
-  | intro a₁ key1 =>
-    cases key2 with 
-      | intro a₂ key2 =>
-        rw [← key1, ← key2]
-        rw [h1.right a₁, h1.right a₂]
-        rw [← key1, ← key2 ] at difb₁b₂
-        exact deterministic_function f a₁ a₂ difb₁b₂
+lemma reciprocal_of_bij_is_bij {α : Type _} {β : Type _} (f : α -> β) (f_inv : β -> α) (h1 : is_reciprocal f f_inv) (h2 : is_bijective f) : is_bijective f_inv := by
+constructor
+{
+  intros b₁ b₂ difb₁b₂
+  have key1 : ∃ (a : α), f a = b₁ := h2.right b₁
+  have key2 : ∃ (a : α), f a = b₂ := h2.right b₂
+  cases key1 with 
+    | intro a₁ key1 =>
+      cases key2 with 
+        | intro a₂ key2 =>
+          rw [← key1, ← key2]
+          rw [h1.right a₁, h1.right a₂]
+          rw [← key1, ← key2 ] at difb₁b₂
+          exact deterministic_function f a₁ a₂ difb₁b₂
+}
 
-
+{
+  intro a
+  use (f a)
+  exact h1.right a
+}
 /- def is_reciprocal {α : Type _} {β : Type _} (f : α -> β) (f_inv : β -> α) := (∀ (b : β), f (f_inv b) = b ∧ ∀ (a : α), f_inv (f a) = a)
 
 lemma deterministic_function {α : Type _} {β : Type _} (f : α -> β) : ∀ (a₁ a₂ : α), f a₁ ≠ f a₂ -> a₁ ≠ a₂ := by
--- GOSVGDProofs/PushForward.lean
+++ GOSVGDProofs/PushForward.lean
@@ -1,19 +1,43 @@
 import Mathlib.Tactic
 import Mathlib.MeasureTheory.Measure.Lebesgue.Basic
+import Mathlib.MeasureTheory.Measure.ProbabilityMeasure
+import Mathlib.MeasureTheory.Integral.Average
+import Mathlib.Init.Set
 import GOSVGDProofs.Bijection
+import GOSVGDProofs.AbsoluteContinuity
 open ENNReal
 
 namespace MeasureTheory
 
 structure Pushforward_Measure (α : Type _) (β : Type _) [MeasurableSpace α] [MeasurableSpace β] extends Measure β where
+p_μ : Measure β
 μ : Measure α
-f : Set α -> Set β
-f_inv : Set β -> Set α
+f : α -> β
+f_inv : β -> α
 is_bij : is_bijective f
 is_reci : is_reciprocal f f_inv
 
-instance Pushforward_Measure.instCoeFun {α : Type _} {β : Type _} [MeasurableSpace α] [MeasurableSpace β] : CoeFun (Pushforward_Measure α β) fun _ => Set β → ℝ≥0∞ :=
+variable (α : Type _) (β : Type _) (h1 : MeasurableSpace α) (h2 : MeasurableSpace β)
+variable (ν : Measure α) -- Lebesgue
+variable (μ_t : Pushforward_Measure α α) (h1 : IsProbabilityMeasure μ_t.p_μ) (h2 : IsProbabilityMeasure μ_t.μ)
+
+noncomputable def pushforward_average (μ : Pushforward_Measure α β) [IsProbabilityMeasure μ.p_μ] [IsProbabilityMeasure μ.μ] (f : β -> ℝ≥0∞) := laverage μ.p_μ f = ∫⁻ x, f (μ.f x) ∂(μ.μ)
+
+lemma has_density {μ ν : Measure α} [IsProbabilityMeasure μ] [IsProbabilityMeasure ν] (h : absolutely_continuous μ ν) : ∃ (d : α -> ℝ≥0∞), ∀ (s : Set α), μ s = ∫⁻ x in s, d x ∂ν := by
+-- Radon-Nikodym
+sorry
+
+def is_density {α : Type _} [MeasurableSpace α] (μ : Measure α) (ν : Measure α) (d : α -> ℝ≥0∞) := ∀ (s : Set α), μ s = ∫⁻ x in s, d x ∂ν
+
+variable (f : Set α -> Set α)
+
+lemma push_forward_density_equality (h1 : absolutely_continuous μ_t.μ ν) (f_mu : α -> ℝ≥0∞) (f_p_mu : α -> ℝ≥0∞) (hf1 : is_density μ_t.μ ν f_mu) (hf2 : is_density μ_t.p_μ ν f_p_mu) : ∀ (s : Set α), ∫⁻ x in s, f_mu x ∂ν = ∫⁻ x in (f s), f_p_mu x ∂ν := by
+
+sorry
+
+
+/- instance Pushforward_Measure.instCoeFun {α : Type _} {β : Type _} [MeasurableSpace α] [MeasurableSpace β] : CoeFun (Pushforward_Measure α β) fun _ => Set β → ℝ≥0∞ :=
   ⟨fun m => m.μ.toOuterMeasure ∘ m.f_inv⟩
 
 variable (α : Type _) (β : Type _) (h1 : MeasurableSpace α) (h2 : MeasurableSpace β) (μ : Pushforward_Measure α β) (s : Set β)
-#check μ s
+#check μ s -/

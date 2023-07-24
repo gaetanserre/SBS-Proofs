111ccad8e91bb5dbb02893bc57d6a344fa112c9a
Gaëtan Serré <gaetan.serre@ens-paris-saclay.fr>
2023-07-24 19:58:50 +0200

    Much better push_forward density def

--- GOSVGDProofs/Proofs.lean
+++ GOSVGDProofs/Proofs.lean
@@ -16,9 +16,8 @@ variable (μ_t : Pushforward_Measure α α) {is_prob_m_μ : IsProbabilityMeasure
 variable {habs1 : absolutely_continuous μ_t.μ ν}
 variable (T' : α → α →L[ℝ] α) (hT' : ∀ (s : Set α), ∀ x ∈ s, HasFDerivWithinAt μ_t.T_inv (T' x) s x) (d_μ : α → ℝ≥0∞) (h_density_μ : is_density μ_t.μ ν d_μ)
 
-noncomputable def push_forward_density (μ_t : Pushforward_Measure α α) (d_μ : α → ℝ≥0∞) (_h : is_density μ_t.μ ν d_μ) (T' : α → α →L[ℝ] α) (_h2 : ∀ (s : Set α), ∀ x ∈ s, HasFDerivWithinAt μ_t.T_inv (T' x) s x) (x : α) := ENNReal.ofReal |(T' x).det| * d_μ (μ_t.T_inv x)
 
-lemma push_forward_has_density (h1 : is_density μ_t.μ ν d_μ) (h2 : ∀ (s : Set α), MeasurableSet s) : is_density μ_t.p_μ ν (push_forward_density μ_t d_μ h1 T' hT') :=
+lemma push_forward_has_density (h1 : is_density μ_t.μ ν d_μ) (h2 : ∀ (s : Set α), MeasurableSet s) : is_density μ_t.p_μ ν (fun x => ENNReal.ofReal |(T' x).det| * d_μ (μ_t.T_inv x)) :=
 by
 unfold is_density
 intro A
@@ -33,8 +32,6 @@ have T_invisInjonA : Set.InjOn μ_t.T_inv A := by
 rw [μ_t.measure_app]
 rw [h1 (μ_t.T_inv '' A)]
 rw [lintegral_image_eq_lintegral_abs_det_fderiv_mul ν (h2 A) (hT' A) T_invisInjonA d_μ]
-unfold push_forward_density
-simp
 
 variable (s : Set α) (s_unique_diff : UniqueDiffOn ℝ s)
 lemma det_of_derivative_of_composition_of_reciprocal_eq_1 (f : α → β) (f_inv : β → α) (h1 : is_reciprocal f f_inv) (h2 : ∀ x ∈ s, HasFDerivWithinAt (f_inv ∘ f) (T' x) s x) : ∀ (a : α), a ∈ s → (T' a).det = 1 :=
@@ -62,9 +59,13 @@ rw [key]
 unfold ContinuousLinearMap.det
 simp
 
-def push_forward_density_composition (f : α → α) (T_compose' : α → α →L[ℝ] α) (_h :  ∀ x ∈ s, HasFDerivWithinAt (μ_t.T_inv ∘ f) (T_compose' x) s x) := ∀ (x : α), ((push_forward_density μ_t d_μ h_density_μ T' hT') ∘ f) x = ENNReal.ofReal |(T_compose' x).det| * d_μ ((μ_t.T_inv ∘ f) x)
+def is_push_forward_density (μ_t : Pushforward_Measure α α) {d_μ : α → ℝ≥0∞} (_h1 : is_density μ ν d_μ) {T' : α → α →L[ℝ] α} (_hT' : ∀ (s : Set α), ∀ x ∈ s, HasFDerivWithinAt μ_t.T_inv (T' x) s x) (d_p_μ : α → ℝ≥0∞) := ∀ (x : α), d_p_μ x = ENNReal.ofReal |(T' x).det| * d_μ (μ_t.T_inv x)
 
-lemma push_forward_density_equality (T_compose' : α → α →L[ℝ] α) (h : ∀ x ∈ s, HasFDerivWithinAt (μ_t.T_inv ∘ μ_t.T) (T_compose' x) s x) (h2 : push_forward_density_composition μ_t T' hT' d_μ h_density_μ s μ_t.T T_compose' h) : ∀ (a : α), a ∈ s → ((push_forward_density μ_t d_μ h_density_μ T' hT') ∘ μ_t.T) a  = d_μ a :=
+variable (d_p_μ : α → ℝ≥0∞) (h_d_p_μ : is_push_forward_density μ_t h_density_μ hT' d_p_μ)
+
+def push_forward_density_composition (f : α → α) (T_compose' : α → α →L[ℝ] α) (_h1 :  ∀ x ∈ s, HasFDerivWithinAt (μ_t.T_inv ∘ f) (T_compose' x) s x) (_h2 : is_push_forward_density μ_t h_density_μ hT' d_p_μ) := ∀ (x : α), (d_p_μ ∘ f) x = ENNReal.ofReal |(T_compose' x).det| * d_μ ((μ_t.T_inv ∘ f) x)
+
+lemma push_forward_density_equality (T_compose' : α → α →L[ℝ] α) (h : ∀ x ∈ s, HasFDerivWithinAt (μ_t.T_inv ∘ μ_t.T) (T_compose' x) s x) (h2 : push_forward_density_composition μ_t T' hT' d_μ h_density_μ s d_p_μ μ_t.T T_compose' h h_d_p_μ) : ∀ (a : α), a ∈ s → (d_p_μ ∘ μ_t.T) a  = d_μ a :=
 by
 intro a ainS
 rw [h2]
@@ -75,51 +76,45 @@ simp
 variable (π : Measure α) (d_π : α → ℝ≥0∞)
 variable (π_t : Pushforward_Measure α α) (h_pi_T : π_t.T = μ_t.T_inv) (h_pi_μ : π_t.μ = π) (h_density_π : is_density π_t.μ ν d_π) [IsProbabilityMeasure π_t.μ] [IsProbabilityMeasure π_t.p_μ]
 
-noncomputable def d_p := push_forward_density μ_t d_μ h_density_μ T' hT'
-variable (h_density_p_μ : is_density μ_t.p_μ ν (d_p μ_t T' hT' d_μ h_density_μ))
-
 variable (Tπ' : α → α →L[ℝ] α) (hTπ' : ∀ (s : Set α), ∀ x ∈ s, HasFDerivWithinAt π_t.T_inv (Tπ' x) s x)
-variable (h_density_p_π : is_density π_t.p_μ ν (d_p π_t Tπ' hTπ' d_π h_density_π))
+variable (d_p_π : α → ℝ≥0∞) (h_d_p_π : is_push_forward_density π_t h_density_π hTπ' d_p_π)
 
 noncomputable def log (a : ℝ≥0∞) := Real.log (ENNReal.toReal a)
 
 noncomputable def KL (μ : Measure α) (d_μ : α → ℝ≥0∞) (d_π : α → ℝ≥0∞) := ∫ x in Set.univ, log ((d_μ x) / (d_π x)) ∂μ
 
 variable (univ_unique_diff : UniqueDiffOn ℝ (Set.univ : Set α))
-example (Tμ_comp' : α → α →L[ℝ] α) (Tπ_comp' : α → α →L[ℝ] α) (h1 : ∀ x ∈ Set.univ, HasFDerivWithinAt (μ_t.T_inv ∘ μ_t.T) (Tμ_comp' x) Set.univ x) (h2 : ∀ x ∈ Set.univ, HasFDerivWithinAt (π_t.T_inv ∘ π_t.T) (Tπ_comp' x) Set.univ x) (h3 : push_forward_density_composition μ_t T' hT' d_μ h_density_μ Set.univ μ_t.T Tμ_comp' h1) (h4 : push_forward_density_composition π_t Tπ' htπ' d_π h_density_π Set.univ π_t.T Tπ_comp' h2) : KL μ_t.p_μ (d_p μ_t T' hT' d_μ h_density_μ) d_π = KL μ_t.μ d_μ (d_p π_t Tπ' hTπ' d_π h_density_π) :=
+lemma KL_of_μ_t_π_eq_KL_of_π_t_μ (Tμ_comp' : α → α →L[ℝ] α) (Tπ_comp' : α → α →L[ℝ] α) (h1 : ∀ x ∈ Set.univ, HasFDerivWithinAt (μ_t.T_inv ∘ μ_t.T) (Tμ_comp' x) Set.univ x) (h2 : ∀ x ∈ Set.univ, HasFDerivWithinAt (π_t.T_inv ∘ π_t.T) (Tπ_comp' x) Set.univ x) (h3 : push_forward_density_composition μ_t T' hT' d_μ h_density_μ Set.univ d_p_μ μ_t.T Tμ_comp' h1 h_d_p_μ) (h4 : push_forward_density_composition π_t Tπ' hTπ' d_π h_density_π Set.univ d_p_π π_t.T Tπ_comp' h2 h_d_p_π) : KL μ_t.p_μ d_p_μ d_π = KL μ_t.μ d_μ d_p_π :=
 by
---rw [KL_μ_t_π]
 unfold KL
 rw [μ_t.integration]
 
-have k_μ : (fun x => log (d_p μ_t T' hT' d_μ h_density_μ x / d_π x)) ∘ μ_t.T = (fun x => log ((d_μ x) / ((d_π ∘ μ_t.T) x)) ):= by
+have k_μ : (fun x => log (d_p_μ x / d_π x)) ∘ μ_t.T = (fun x => log ((d_μ x) / ((d_π ∘ μ_t.T) x)) ):= by
 {
-  have k : ∀ (a : α), (d_p μ_t T' hT' d_μ h_density_μ ∘ μ_t.T) a = d_μ a := by 
+  have k : ∀ (a : α), (d_p_μ  ∘ μ_t.T) a = d_μ a := by 
   {
-    have key := push_forward_density_equality μ_t T' hT' d_μ h_density_μ Set.univ univ_unique_diff Tμ_comp' h1 h3
+    have key := push_forward_density_equality μ_t T' hT' d_μ h_density_μ Set.univ univ_unique_diff d_p_μ h_d_p_μ Tμ_comp' h1 h3
     intro a
     exact key a (by simp)
   }
   conv =>
     rhs
     intro x
-    rw [←k]
+    rw [←k x]
 }
 
 rw [k_μ]
 rw [image_of_univ_is_univ μ_t.T μ_t.T_inv μ_t.is_bij μ_t.is_reci]
 
-have k_π : (d_p π_t Tπ' hTπ' d_π h_density_π) = (d_π ∘ μ_t.T) := by 
+have k_π : d_p_π = (d_π ∘ μ_t.T) := by 
 {
-  have key := push_forward_density_equality π_t Tπ' hTπ' d_π h_density_π Set.univ univ_unique_diff Tπ_comp' h2 h4
+  have key := push_forward_density_equality π_t Tπ' hTπ' d_π h_density_π Set.univ univ_unique_diff d_p_π h_d_p_π Tπ_comp' h2 h4
   ext a
   unfold Function.comp at key
   unfold Function.comp
   rw [←key (μ_t.T a) (by simp)]
   rw [h_pi_T]
   rw [μ_t.is_reci.right a]
-  unfold d_p
-  simp
 }
 
 rw [k_π]

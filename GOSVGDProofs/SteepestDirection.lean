035dbb32d64adc97584e905ef94ad4d8a56465d9
Gaëtan Serré <gaetan.serre@ens-paris-saclay.fr>
2023-08-02 12:08:19 +0200

    Last proofs explained

--- GOSVGDProofs/SteepestDirection.lean
+++ GOSVGDProofs/SteepestDirection.lean
@@ -74,7 +74,7 @@ def norm_H (H : Set (ℕ → (Vector ℝ d) → ℝ)) := ∀ f ∈ H, ∀x, (‖
 /--
   For all non-empty finite set s, ∃ e ∈ s, ∀ a ∈ s, a ≤ e.
 -/
-lemma exist_max_finset {ι : Type _} [LinearOrder ι] (s : Finset ι) (h : Finset.Nonempty s) : ∃ e ∈ s, ∀ a ∈ s, a ≤ e :=
+theorem exist_max_finset {ι : Type _} [LinearOrder ι] (s : Finset ι) (h : Finset.Nonempty s) : ∃ e ∈ s, ∀ a ∈ s, a ≤ e :=
 by
   use (Finset.max' s h)
   constructor
@@ -87,7 +87,7 @@ by
 /--
   Given a non-empty finite set s and a function f on elements of s, ∃ j ∈ s, ∀ i ∈ s, f i ≤ f j.
 -/
-lemma exist_max_image_finset {ι E : Type _} [LinearOrder E] (s : Finset ι) (h : Finset.Nonempty s) (f : ι → E) : ∃ j ∈ s, ∀ i ∈ s, f i ≤ f j :=
+theorem exist_max_image_finset {ι E : Type _} [LinearOrder E] (s : Finset ι) (h : Finset.Nonempty s) (f : ι → E) : ∃ j ∈ s, ∀ i ∈ s, f i ≤ f j :=
 by 
   let sf := Finset.image f s
   have hf : Finset.Nonempty sf := Nonempty.image h f
@@ -109,48 +109,48 @@ by
 /--
   a*a = a²
 -/
-lemma square {M : Type _} [Monoid M] (a : M) : a * a = a^2 := (sq a).symm
+theorem square {M : Type _} [Monoid M] (a : M) : a * a = a^2 := (sq a).symm
 
 /--
   a² * b² = (a * b)²
 -/
-lemma distrib_sq {M : Type _} [CommMonoid M] (a b : M) : a^2 * b^2 = (a * b)^2 := (mul_pow a b 2).symm
+theorem distrib_sq {M : Type _} [CommMonoid M] (a b : M) : a^2 * b^2 = (a * b)^2 := (mul_pow a b 2).symm
 /--
   ∀ a b ∈ ℝ⁺ ∪ {∞}, a ≤ b → a² ≤ b²
 -/
-lemma le_square {a b : ℝ≥0∞} (h : a ≤ b) : a^2 ≤ b^2 :=
+theorem le_square {a b : ℝ≥0∞} (h : a ≤ b) : a^2 ≤ b^2 :=
 by
   have k := mul_le_mul h h (by simp) (by simp)
   rwa [←square a, ←square b]
 
-/- Coercion lemmas -/
+/- Coercion theorems -/
 
-lemma le_coe (a : ℝ) (b : NNReal) (h1 : 0 ≤ a) : ‖a‖₊ ≤ b → ENNReal.ofReal a ≤ ENNReal.ofReal b :=
+theorem le_coe (a : ℝ) (b : NNReal) (h1 : 0 ≤ a) : ‖a‖₊ ≤ b → ENNReal.ofReal a ≤ ENNReal.ofReal b :=
 by
   intro h
   have k := Real.ennnorm_eq_ofReal h1
   rw [←k]
   rwa [ENNReal.ofReal_coe_nnreal, ENNReal.coe_le_coe]
 
-lemma coe_nnreal_le {a b : ℝ≥0} (h : a ≤ b) : (a : ℝ≥0∞) ≤ (b : ℝ≥0∞) := Iff.mpr coe_le_coe h
+theorem coe_nnreal_le {a b : ℝ≥0} (h : a ≤ b) : (a : ℝ≥0∞) ≤ (b : ℝ≥0∞) := Iff.mpr coe_le_coe h
 
-lemma coe_distrib (a b : ℝ≥0) : ENNReal.some (a * b) = (a : ℝ≥0∞) * (b : ℝ≥0∞) := ENNReal.coe_mul
+theorem coe_distrib (a b : ℝ≥0) : ENNReal.some (a * b) = (a : ℝ≥0∞) * (b : ℝ≥0∞) := ENNReal.coe_mul
 
-lemma nn_norm_eq_norm (a : (Vector ℝ d) → ℝ) : ‖a‖₊ = ENNReal.ofReal ‖a‖ := (ofReal_norm_eq_coe_nnnorm a).symm
+theorem nn_norm_eq_norm (a : (Vector ℝ d) → ℝ) : ‖a‖₊ = ENNReal.ofReal ‖a‖ := (ofReal_norm_eq_coe_nnnorm a).symm
 
-lemma nn_norm_eq_norm_re (a : ℝ) : ‖a‖₊ = ENNReal.ofReal ‖a‖ := (ofReal_norm_eq_coe_nnnorm a).symm
+theorem nn_norm_eq_norm_re (a : ℝ) : ‖a‖₊ = ENNReal.ofReal ‖a‖ := (ofReal_norm_eq_coe_nnnorm a).symm
 
-lemma enn_square {a : ℝ} (h : 0 ≤ a) : ENNReal.ofReal (a) ^ 2 = ENNReal.ofReal (a ^ 2) :=
+theorem enn_square {a : ℝ} (h : 0 ≤ a) : ENNReal.ofReal (a) ^ 2 = ENNReal.ofReal (a ^ 2) :=
 by
   rw [←square (ENNReal.ofReal (a)), ←square a]
   exact (ofReal_mul h).symm
 
-lemma nn_square {a : ℝ≥0} : (a : ℝ≥0∞) ^ 2 = (a ^ 2 : ℝ≥0∞) := (ENNReal.coe_pow 2).symm
+theorem nn_square {a : ℝ≥0} : (a : ℝ≥0∞) ^ 2 = (a ^ 2 : ℝ≥0∞) := (ENNReal.coe_pow 2).symm
 
 /--
   A finite sum of finite elements is finite.
 -/
-lemma finite_sum (f : ℕ → ℝ≥0) : ∃ (C : ℝ≥0), ∑ i in range (d + 1), (f i : ℝ≥0∞) < ENNReal.some C :=
+theorem finite_sum (f : ℕ → ℝ≥0) : ∃ (C : ℝ≥0), ∑ i in range (d + 1), (f i : ℝ≥0∞) < ENNReal.some C :=
 by
   /- We begin to show that each element of the sum is bounded from above. -/
   have sup_el : ∀ i ∈ range (d + 1), ∃ c, (f i) < c := fun i _ ↦ exists_gt (f i)
@@ -210,8 +210,6 @@ def integral_is_finite (μ : Measure (Vector ℝ d)) (f : (Vector ℝ d) → ℝ
 /--
   H ⊆ L2(μ) i.e., ∀ f ∈ H ∫⁻ x in Set.univ, ∑ i in range (d + 1), ENNReal.ofReal (|f i x|)^2 ∂μ < ∞.
 -/
-example (f : ℕ → (Vector ℝ d) → ℝ) (x : (Vector ℝ d)) : ENNReal.ofReal ‖fun i ↦ f i x‖ = ‖fun i ↦ f i x‖₊ := ofReal_norm_eq_coe_nnnorm fun i => f i x
-
 theorem H_subset_of_L2 (μ : Measure (Vector ℝ d)) (h1 : product_RKHS H H₀) (h2 : integral_is_finite μ (fun x ↦ k x x)) (h3 : norm_H H) : ∀ f ∈ H, ∫⁻ x in Set.univ, ENNReal.ofReal ‖fun i ↦ f i x‖^2 ∂μ < ∞ :=
 by
   intros f finH
@@ -385,14 +383,14 @@ variable (dk : (Vector ℝ d) → ℕ → (Vector ℝ d) → ℝ)
 /--
   Linearity of inner product applied to integral
 -/
-lemma inter_inner_integral_right (μ : Measure (Vector ℝ d)) (g : (Vector ℝ d) → ℝ) (f : (Vector ℝ d) → (Vector ℝ d) → ℝ) : ⟪g, (fun x ↦ (∫ y, f y x ∂μ))⟫ = ∫ y, ⟪g, f y⟫ ∂μ :=
+theorem inter_inner_integral_right (μ : Measure (Vector ℝ d)) (g : (Vector ℝ d) → ℝ) (f : (Vector ℝ d) → (Vector ℝ d) → ℝ) : ⟪g, (fun x ↦ (∫ y, f y x ∂μ))⟫ = ∫ y, ⟪g, f y⟫ ∂μ :=
 by
 sorry
 
 /--
   Linearity of inner product for function
 -/
-lemma inner_linear_left (f a b : Vector ℝ d → ℝ) (c : ℝ) : ⟪f, fun x ↦ c * a x + b x⟫ = c * ⟪f, fun x ↦ a x⟫ + ⟪f, fun x ↦ b x⟫ := by sorry
+theorem inner_linear_left (f a b : Vector ℝ d → ℝ) (c : ℝ) : ⟪f, fun x ↦ c * a x + b x⟫ = c * ⟪f, fun x ↦ a x⟫ + ⟪f, fun x ↦ b x⟫ := by sorry
 
 /--
 ⟪f, ∇k(x, ̇)⟫ = ∇f(x)
@@ -405,9 +403,9 @@ by
 /--
   Linearity of inner product for function
 -/
-lemma inner_linear_right (f a b : ℕ → Vector ℝ d → ℝ) (c : ℝ) : ⟪fun i x ↦ c * a i x + b i x, f⟫ = c * ⟪fun i x ↦ a i x, f⟫ + ⟪fun i x ↦ b i x, f⟫ := by sorry
+theorem inner_linear_right (f a b : ℕ → Vector ℝ d → ℝ) (c : ℝ) : ⟪fun i x ↦ c * a i x + b i x, f⟫ = c * ⟪fun i x ↦ a i x, f⟫ + ⟪fun i x ↦ b i x, f⟫ := by sorry
 
-lemma inner_zero (a : ℕ → Vector ℝ d → ℝ) : ⟪0, a⟫ = 0 := by sorry
+theorem inner_zero (a : ℕ → Vector ℝ d → ℝ) : ⟪0, a⟫ = 0 := by sorry
 
 /-==============-/
 
@@ -474,7 +472,10 @@ by
 -/
 lemma bound_direction (h1 : product_RKHS H H₀) (h2 : inner_product_H H) (f : ℕ → (Vector ℝ d) → ℝ) (hf : f ∈ H) (hfb : ‖f‖ = 1) (df : ℕ → (Vector ℝ d) → ℝ) : ∫ x, ∑ l in range (d + 1), ((d_log_π l x) * (f l x) + df l x) ∂μ ≤ ‖ϕ‖ :=
 by
+  /- We rewrite ∫ x, ∑ l in range (d + 1), ((d_log_π l x) * (f l x) + df l x) as ⟪f, ϕ⟫. -/
   rw [←inner_product_eq_dKL μ H₀ k h_kernel dk d_log_π ϕ hϕ h_is_ϕ is_integrable_H h1 h2 f hf df]
+
+  /- We use Cauchy-Schwarz inequality. -/
   calc ⟪f, ϕ⟫ ≤ ‖⟪f, ϕ⟫‖ := le_abs_self ⟪f, ϕ⟫
   _ ≤ ‖f‖ * ‖ϕ‖ := norm_inner_le_norm f ϕ
   _ = ‖ϕ‖ := by {
@@ -489,9 +490,11 @@ theorem steepest_descent_trajectory (h1 : product_RKHS H H₀) (h2 : inner_produ
 by
   rw [←inner_product_eq_dKL μ H₀ k h_kernel dk d_log_π ϕ hϕ h_is_ϕ is_integrable_H h1 h2 (fun i x ↦ ϕ i x / ‖ϕ‖) hϕs dϕ]
 
+  /- We rewrite the division as a product of inverse. -/
   have div_to_mul : ∀i, ∀x, ϕ i x / ‖ϕ‖ = ϕ i x * (1 / ‖ϕ‖) := fun i x ↦ div_eq_mul_one_div (ϕ i x) ‖ϕ‖
   simp_rw [div_to_mul]
 
+  /- We use the linearity of the scalar product to get 1 / ‖ϕ‖ out. -/
   have linear_inner : ⟪(fun i x => ϕ i x * (1 / ‖ϕ‖)), ϕ⟫ = 1 / ‖ϕ‖ * ⟪(fun i x => ϕ i x), ϕ⟫ + ⟪(fun i x => 0), ϕ⟫ := by {
     have comm : ∀i, ∀x, (1 / ‖ϕ‖) * (ϕ i x) = (ϕ i x) * (1 / ‖ϕ‖) := fun i x ↦ mul_comm (1 / ‖ϕ‖) (ϕ i x)
     simp_rw [←comm]
@@ -501,11 +504,13 @@ by
   }
   rw [linear_inner]
 
+  /- We use the fact that ⟪0, f⟫ = 0. -/
   have inner_prod_zero : ⟪fun i x ↦ 0, ϕ⟫ = 0 := by {
     exact inner_zero ϕ
   }
   rw[inner_prod_zero, add_zero]
 
+  /- We use the theorem *inner_self_eq_norm_mul_norm* stating that re ⟪a, a⟫ = ‖a‖ * ‖a‖. -/
   have eq_re : ⟪fun i x ↦ ϕ i x, ϕ⟫ = re ⟪ϕ, ϕ⟫ := by simp
   rw [eq_re]
   rw [inner_self_eq_norm_mul_norm]
@@ -523,7 +528,7 @@ noncomputable def log (a : ℝ≥0∞) := Real.log (ENNReal.toReal a)
 /--
  ∀ a ∈ ]0, ∞[, exp (log a) = (a : ℝ).
 -/
-lemma enn_comp_exp_log (a : ℝ≥0∞) (ha : a ≠ 0) (ha2 : a ≠ ∞) : Real.exp (log a) = ENNReal.toReal a := by
+theorem enn_comp_exp_log (a : ℝ≥0∞) (ha : a ≠ 0) (ha2 : a ≠ ∞) : Real.exp (log a) = ENNReal.toReal a := by
   by_cases ENNReal.toReal a = 0
   {
     exfalso
@@ -547,7 +552,7 @@ lemma enn_comp_exp_log (a : ℝ≥0∞) (ha : a ≠ 0) (ha2 : a ≠ ∞) : Real.
 /--
  ∀ a ∈ ]0, ∞[, log a = (c : ℝ) → a = (exp c : ℝ≥0∞).
 -/
-lemma cancel_log_exp (a : ℝ≥0∞) (ha : a ≠ 0) (ha2 : a ≠ ∞) (c : ℝ) : log a = c → a = ENNReal.ofReal (Real.exp c) :=
+theorem cancel_log_exp (a : ℝ≥0∞) (ha : a ≠ 0) (ha2 : a ≠ ∞) (c : ℝ) : log a = c → a = ENNReal.ofReal (Real.exp c) :=
 by
   intro h
   rw [←h, enn_comp_exp_log a ha ha2]
@@ -561,7 +566,7 @@ variable [Norm (Vector ℝ d)]
 /--
 Unformal but highly pratical multivariate integration by parts.
 -/
-lemma mv_integration_by_parts (f : Vector ℝ d → ℝ) (g grad_f dg : ℕ → (Vector ℝ d) → ℝ) (h : ∀ x, tends_to_infty (fun (x : Vector ℝ d) ↦ ‖x‖) → ∀i, f x * g i x = 0) : ∫ x in Set.univ, f x * (∑ i in range (d + 1), dg i x) ∂μ = - ∫ x in Set.univ, (∑ i in range (d + 1), grad_f i x * g i x) ∂μ := by sorry
+theorem mv_integration_by_parts (f : Vector ℝ d → ℝ) (g grad_f dg : ℕ → (Vector ℝ d) → ℝ) (h : ∀ x, tends_to_infty (fun (x : Vector ℝ d) ↦ ‖x‖) → ∀i, f x * g i x = 0) : ∫ x in Set.univ, f x * (∑ i in range (d + 1), dg i x) ∂μ = - ∫ x in Set.univ, (∑ i in range (d + 1), grad_f i x * g i x) ∂μ := by sorry
 
 /- Same as before, we will use this assumption only when the function is trivially integrable (e.g. derivative of integrable functions). -/
 variable (is_integrable_H₀ : ∀ (f : Vector ℝ d → ℝ), Integrable f μ)
@@ -571,7 +576,10 @@ d_log_μ_π : i ↦ c ↦ ∂xⁱ log (μ(x) / π(x))
 -/
 variable (d_log_μ_π : ℕ → (Vector ℝ d) → ℝ)
 
-variable (d_log_μ_π : ℕ → (Vector ℝ d) → ℝ) (hd_log_μ_π : (∀x, ∀i, d_log_μ_π i x = 0) → (∃ c, ∀ x, log (dμ x / dπ x) = c))
+/-
+Simple derivative rule: if the derivative is 0 ∀x, then the function is constant.
+-/
+variable (hd_log_μ_π : (∀x, ∀i, d_log_μ_π i x = 0) → (∃ c, ∀ x, log (dμ x / dπ x) = c))
 
 /-
 dπ' : i ↦ c ↦ ∂xⁱ π(x)
@@ -589,35 +597,42 @@ variable (hπ' : ∀x, ∀i, ENNReal.toReal (dπ x) * d_log_π i x = dπ' i x)
 def SteinClass (f : ℕ → (Vector ℝ d) → ℝ) (dμ : (Vector ℝ d) → ℝ≥0∞) := ∀ x, tends_to_infty (fun (x : Vector ℝ d) ↦ ‖x‖) → ∀i, ENNReal.toReal (dμ x) * f i x = 0
 
 /--
-KSD(μ || π) = ⟪∇log μ/π, Pμ ∇log μ/π⟫_L²(μ). We assume here that KSD is also equal to ∫ x, ∑ l in range (d + 1), (d_log_π l x * ϕ l x + dϕ l x) ∂μ.
+KSD(μ | π) = ⟪∇log μ/π, Pμ ∇log μ/π⟫_L²(μ). We assume here that KSD is also equal to ∫ x, ∑ l in range (d + 1), (d_log_π l x * ϕ l x + dϕ l x) ∂μ.
 -/
-lemma ksd : ∫ x in Set.univ, (∫ x' in Set.univ, (∑ i in range (d + 1), d_log_μ_π i x * k x x' * d_log_μ_π i x') ∂μ) ∂μ = ∫ (x : Vector ℝ d), ∑ l in range (d + 1), (d_log_π l x * ϕ l x + dϕ l x) ∂μ := by sorry
+lemma ksd : ∫ x in Set.univ, (∫ x' in Set.univ, (∑ i in range (d + 1), d_log_μ_π i x * k x x' * d_log_μ_π i x') ∂μ) ∂μ = ∫ x, ∑ l in range (d + 1), (d_log_π l x * ϕ l x + dϕ l x) ∂μ := by sorry
 
 /--
-We show that, if ϕ is in the Stein class of π, KSD is a valid discrepancy measure i.e. μ = π ↔ KSD(μ || π) = 0.
+We show that, if ϕ is in the Stein class of π, KSD is a valid discrepancy measure i.e. μ = π ↔ KSD(μ | π) = 0.
 -/
 lemma KSD_is_valid_discrepancy (hstein : SteinClass ϕ dπ) : μ = π ↔ ∫ x in Set.univ, (∫ x' in Set.univ, (∑ i in range (d + 1), d_log_μ_π i x * k x x' * d_log_μ_π i x') ∂μ) ∂μ = 0 :=
 by
   constructor
   {
+    /- μ = π → KSD(μ | π) = 0. -/
     intro h
 
     rw [ksd μ k d_log_π ϕ dϕ d_log_μ_π]
 
+    -- /- ∑ i, f i + g i = ∑ i, f i + ∑ i, g i. -/
     have split_sum : ∀x, ∑ l in range (d + 1), (d_log_π l x * ϕ l x + dϕ l x) = (∑ l in range (d + 1), d_log_π l x * ϕ l x) + (∑ l in range (d + 1), dϕ l x) := fun x ↦ sum_add_distrib
     simp_rw [split_sum]
 
+    /- Split the integral of sum into sum of integral. -/
     have h1 : Integrable (fun x ↦ (∑ l in range (d + 1), d_log_π l x * ϕ l x)) μ := is_integrable_H₀ _
     have h2 : Integrable (fun x ↦ (∑ l in range (d + 1), dϕ l x)) μ := is_integrable_H₀ _
     rw [integral_add (h1) h2]
 
+    /- Make the `Set.univ` appears for using the density later. -/
     have int_univ : ∫ a, ∑ l in range (d + 1), d_log_π l a * ϕ l a ∂μ = ∫ a in Set.univ, ∑ l in range (d + 1), d_log_π l a * ϕ l a ∂μ := by simp
     rw [int_univ]
 
+    /- Replace μ by π in the integration. -/
     rw [h]
 
+    /- Replace by its density. -/
     rw [density_integration π ν dπ hπ (fun x ↦ (∑ l in range (d + 1), d_log_π l x * ϕ l x)) Set.univ]
 
+    /- Get ENNReal.toReal (dπ x) in the sum (a * ∑ b = ∑ b * a). -/
     have mul_dist : ∀x, ENNReal.toReal (dπ x) * (∑ l in range (d + 1), (fun l ↦ d_log_π l x * ϕ l x) l) = ∑ l in range (d + 1), (fun l ↦ d_log_π l x * ϕ l x) l * ENNReal.toReal (dπ x) := by {
       have mul_dist_sum : ∀ (a : ℝ), ∀ (f : ℕ → ℝ), (∑ i in range (d + 1), f i) * a = ∑ i in range (d + 1), f i * a := fun a f ↦ Finset.sum_mul
       intro x
@@ -626,23 +641,31 @@ by
     }
     simp_rw [mul_dist]
 
+    /- Make the product ENNReal.toReal (dπ x) * d_log_π i x appears to use the log derivative rule. -/
     have mul_comm : ∀x, ∀i, d_log_π i x * ϕ i x * ENNReal.toReal (dπ x) = ENNReal.toReal (dπ x) * d_log_π i x * ϕ i x := fun x i ↦ (mul_rotate (ENNReal.toReal (dπ x)) (d_log_π i x) (ϕ i x)).symm
     simp_rw [mul_comm, hπ']
 
+    /- Make the `Set.univ` appears to use the density. -/
     have int_univ : ∫ a, ∑ l in range (d + 1), dϕ l a ∂π = ∫ a in Set.univ, ∑ l in range (d + 1), dϕ l a ∂π := by simp
     rw [int_univ]
     rw [density_integration π ν dπ hπ (fun x ↦ (∑ l in range (d + 1), dϕ l x)) Set.univ]
 
+    /- Use the integration by parts on the right-hand side integral. -/
     rw [mv_integration_by_parts ν (fun x ↦ ENNReal.toReal (dπ x)) ϕ dπ' dϕ (hstein)]
     simp
   }
   {
+    /- KSD(μ | π) = 0 → μ = π. -/
     intro h
 
+    /- We use the fact that the kernel is positive-definite that implies that d_log_μ_π = 0. -/
     have d_log_μ_π_eq_0 := (h_kernel_positive d_log_μ_π).right.mp h
+
+    /- Simple derivative rule: ∂x f x = 0 → f x = c -/
     specialize hd_log_μ_π d_log_μ_π_eq_0
 
     rcases hd_log_μ_π with ⟨c, h⟩
+    /- We show that, since dμ x / dπ x ≠ 0 and finite, dμ x = ENNReal.ofReal (Real.exp c) * dπ x. -/
     have dμ_propor : ∀x, dμ x = ENNReal.ofReal (Real.exp c) * dπ x := by {
       intro x
       specialize h x
@@ -669,6 +692,7 @@ by
       simp [←cancel_log_exp, ENNReal.div_eq_inv_mul, mul_right_comm (dπ x)⁻¹ (dμ x) (dπ x), ENNReal.inv_mul_cancel (hdπ x).left (hdπ x).right]
     }
 
+    /- We show by cases that ENNReal.ofReal (Real.exp c) = 1. If it is ≠ 1, this implies a contradiction as dμ x = ENNReal.ofReal (Real.exp c) and ∫⁻ x, dμ x ∂ν = 1. -/
     have exp_c_eq_one : ENNReal.ofReal (Real.exp c) = 1 := by {
       by_cases hc : ENNReal.ofReal (Real.exp c) = 1
       {assumption}
@@ -685,14 +709,13 @@ by
         rw [density_lintegration π ν dπ hπ (fun x ↦ 1) Set.univ] at univ_eq_one_π
         simp_rw [mul_one] at univ_eq_one_π
 
-        have const_out_int : ∫⁻ x in Set.univ, ENNReal.ofReal (Real.exp c) * dπ x ∂ν =  ENNReal.ofReal (Real.exp c) * ∫⁻ x in Set.univ, dπ x ∂ν := lintegral_const_mul (ENNReal.ofReal (Real.exp c)) (mdπ)
-
-        rw [const_out_int, univ_eq_one_π, mul_one] at univ_eq_one_μ
+        rw [lintegral_const_mul (ENNReal.ofReal (Real.exp c)) (mdπ), univ_eq_one_π, mul_one] at univ_eq_one_μ
         exfalso
         exact hc univ_eq_one_μ
       }
     }
 
+    /- We rewrite μ = π as ∀s, ∫⁻ x in s, dμ ∂ν = ∀s, ∫⁻ x in s, dπ ∂ν and use dμ = 1 * dπ. -/
     simp_rw [exp_c_eq_one, one_mul] at dμ_propor
     ext s _hs
     rw [←set_lintegral_one s, ←set_lintegral_one s]

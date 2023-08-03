9b3e711794fd16e1819dab500fbbee901a782854
Gaëtan Serré <gaetan.serre@ens-paris-saclay.fr>
2023-08-03 16:27:54 +0200

    Check build

--- GOSVGDProofs.lean
+++ GOSVGDProofs.lean
@@ -1,1 +1,7 @@
+import GOSVGDProofs.AbsoluteContinuity
+import GOSVGDProofs.Bijection
+--import GOSVGDProofs.KL
+import GOSVGDProofs.PushForward
+import GOSVGDProofs.SteepestDirection
+
 def hello := "world"

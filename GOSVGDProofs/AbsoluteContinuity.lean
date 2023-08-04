a6cb9eb7657d65584c2143b4d6be61ceeae0c0ab
Gaëtan Serré <gaetan.serre@ens-paris-saclay.fr>
2023-08-04 17:34:54 +0200

    Implicit measures

--- GOSVGDProofs/AbsoluteContinuity.lean
+++ GOSVGDProofs/AbsoluteContinuity.lean
@@ -4,7 +4,7 @@ namespace MeasureTheory
 
 def absolutely_continuous {α : Type _} {_m : MeasurableSpace α} (μ ν : Measure α) := ∀ (s : Set α), ν s = 0 → μ s = 0
 
-lemma absolutely_continuous_trans {α : Type _} {_m : MeasurableSpace α} (μ₁ μ₂ μ₃ : Measure α) (h1: absolutely_continuous μ₁ μ₂) (h2: absolutely_continuous μ₂ μ₃) : absolutely_continuous μ₁ μ₃ := by
+lemma absolutely_continuous_trans {α : Type _} {_m : MeasurableSpace α} {μ₁ μ₂ μ₃ : Measure α} (h1: absolutely_continuous μ₁ μ₂) (h2: absolutely_continuous μ₂ μ₃) : absolutely_continuous μ₁ μ₃ := by
   intros s hsigma
   specialize h1 s
   specialize h2 s

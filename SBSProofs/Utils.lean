a52be96dc4baf9a0ec1deae1391d6d954b5b55fb
Gaëtan Serré <gaetan.serre@ens-paris-saclay.fr>
2024-03-21 17:58:26 +0100

    Removed useless results in Utils.lean

--- SBSProofs/Utils.lean
+++ SBSProofs/Utils.lean
@@ -56,11 +56,6 @@ by
   exact max (mem_image_of_mem f iin)
 
 /--
-  a*a = a²
--/
-lemma square {M : Type _} [Monoid M] (a : M) : a * a = a^2 := (sq a).symm
-
-/--
   a² * b² = (a * b)²
 -/
 lemma distrib_sq {M : Type _} [CommMonoid M] (a b : M) : a^2 * b^2 = (a * b)^2 := (mul_pow a b 2).symm
@@ -70,16 +65,9 @@ lemma distrib_sq {M : Type _} [CommMonoid M] (a b : M) : a^2 * b^2 = (a * b)^2 :
 lemma le_square {a b : ℝ≥0∞} (h : a ≤ b) : a^2 ≤ b^2 :=
 by
   have k := mul_le_mul h h (by simp) (by simp)
-  rwa [←square a, ←square b]
+  rwa [pow_two a, pow_two b]
 
 /- Coercion lemmas -/
-
-lemma le_coe (a : ℝ) (b : NNReal) (h1 : 0 ≤ a) : ‖a‖₊ ≤ b → ENNReal.ofReal a ≤ ENNReal.ofReal b :=
-by
-  intro h
-  have k := Real.ennnorm_eq_ofReal h1
-  rw [←k]
-  rwa [ENNReal.ofReal_coe_nnreal, ENNReal.coe_le_coe]
 
 lemma coe_nnreal_le {a b : ℝ≥0} (h : a ≤ b) : (a : ℝ≥0∞) ≤ (b : ℝ≥0∞) := Iff.mpr coe_le_coe h
 
@@ -89,7 +77,7 @@ lemma nn_norm_eq_norm_re (a : ℝ) : ‖a‖₊ = ENNReal.ofReal ‖a‖ := (ofR
 
 lemma enn_square {a : ℝ} (h : 0 ≤ a) : ENNReal.ofReal (a) ^ 2 = ENNReal.ofReal (a ^ 2) :=
 by
-  rw [←square (ENNReal.ofReal (a)), ←square a]
+  rw [pow_two (ENNReal.ofReal (a)), pow_two a]
   exact (ofReal_mul h).symm
 
 
@@ -184,15 +172,9 @@ lemma inner_linear_right (f a b : ℕ → Vector ℝ d → ℝ) (c : ℝ) : ⟪f
 
 lemma inner_zero (a : ℕ → Vector ℝ d → ℝ) : ⟪0, a⟫ = 0 := by sorry
 
-variable [MeasureSpace ℝ≥0] [NormedAddCommGroup ℝ≥0∞] [NormedSpace ℝ ℝ≥0∞] [LocallyFiniteOrder ℝ≥0]
-
-lemma pos_integral (f : ℝ≥0 → ℝ≥0∞) : ∀ (t : ℝ≥0), 0 < t → (∀ s, 0 < f s) → 0 < ∫ s in Icc 0 t, f s := by sorry
-
-lemma finite_integral (f : ℝ≥0 → ℝ≥0∞) : ∀ (t : ℝ≥0), (∀ s, f s ≠ ∞) → ∫ s in Icc 0 t, f s ≠ ∞ := by sorry
-
-lemma coe_integral (f : ℝ≥0 → ℝ≥0∞) : ∀ (t : ℝ≥0), ∫ s in Icc 0 t, ENNReal.toReal (f s) = ENNReal.toReal (∫ s in Icc 0 t, f s) := by sorry
-
 /-==============-/
+
+variable [MeasureSpace ℝ≥0] [NormedAddCommGroup ℝ≥0∞] [NormedSpace ℝ ℝ≥0∞] [LocallyFiniteOrder ℝ≥0]
 
 
 /- Def of ℝ≥0∞ coerced log. -/
@@ -244,144 +226,5 @@ variable [Norm (Vector ℝ d)]
   Unformal but highly pratical multivariate integration by parts.
 -/
 theorem mv_integration_by_parts (Ω : Set (Vector ℝ d)) (f : Vector ℝ d → ℝ) (g grad_f dg : ℕ → (Vector ℝ d) → ℝ) (h : ∀ x, tends_to_infty (fun (x : Vector ℝ d) ↦ ‖x‖) → ∀i, f x * g i x = 0) : ∫ x in Ω, f x * (∑ i in range (d + 1), dg i x) ∂μ = - ∫ x in Ω, (∑ i in range (d + 1), grad_f i x * g i x) ∂μ := by sorry
-
-
-noncomputable def exp (a : ℝ) := ENNReal.ofReal (Real.exp a)
-
-lemma lt_eq_le_and_neq : ∀ (a : ℝ), 0 ≤ a ∧ a ≠ 0 ↔ 0 < a :=
-by
-  intro a
-  constructor
-  {
-    intro ha
-    rcases ha with ⟨pos, nneg⟩
-    by_contra ht
-    push_neg at ht
-    have eq_zero : a = 0 := by linarith
-    exact nneg eq_zero
-  }
-  {
-    intro ha
-    exact ⟨le_of_lt ha, ne_of_gt ha⟩
-  }
-
-
-/--
-  Definition of limit at infinity for positive real-valued function. Used in KSD.lean
--/
-def limit (f : ℝ≥0 → ℝ≥0∞) (l : ℝ≥0∞) := ∀ε, 0 < ε → ∃X, ∀x, X ≤ x → |f x - l| < ε
-
-theorem limit_equiv (f : ℝ≥0 → ℝ≥0∞) (l l2 : ℝ≥0∞) : (limit f l ∧ limit f l2) → l = l2 := by sorry
-
-theorem decreasing_bounded_function_limit (f : ℝ≥0 → ℝ≥0∞) (l : ℝ≥0∞) (bounded : ∀x, l ≤ f x) (decreasing : ∀x, ∀y, x < y → f y ≤ f x) : ∃ α, limit f α ∧ l ≤ α ∧ (∀x, α < f x) := sorry
-
-/--
-lim_(t → ∞) β exp(α t) = 0, for any 0 ≤ β and α < 0.
--/
-lemma exp_tends_to_zero : ∀(α : ℝ) (β : ℝ≥0∞), α < 0 → limit (fun t ↦ β * exp (α * t)) 0 := by sorry
-
-/--
-Integral of a constant α over [0, t] is α t.
--/
-theorem integral_of_constant : ∫ s in Icc 0 (t:ℝ≥0), (fun (s : ℝ≥0) ↦ (α : ℝ)) s = α * t := by sorry
-
-/--
-Let f be a decreasing function and g a function s.t. ∃γ, ∀x, γ < g x. Therefore, ∀t, γ/(2*(f 0)) < (g t) / (2*(f t)) (used in KSD.lean).
--/
-lemma decrease_bound (f g : ℝ≥0 → ℝ≥0∞) (decreasing : ∀x, ∀y, x < y → f y ≤ f x) (hf_nn : ∀x, f x ≠ 0) (hf_finite : ∀x, f x ≠ ∞) (γ : ℝ≥0∞) (hg : ∀x, γ < g x) : ∀t, γ / (2*(f 0)) < (g t) / (2*(f t)) :=
-by
-  intro t
-  have h : ∀x, f x ≤ f 0 := by
-  {
-    intro x
-    specialize decreasing 0 x
-    by_cases hx : x = 0
-    {
-      rw [hx]
-    }
-    {
-      push_neg at hx
-      have hxx : 0 < x := Iff.mpr zero_lt_iff hx
-      exact decreasing hxx
-    }
-  }
-  specialize h t
-
-  have f_le : (g t) / (2*(f 0)) ≤ (g t) / (2*(f t)) := ENNReal.div_le_div_left (mul_le_mul_left' h 2) (g t)
-
-  have div_lt : γ / (2*(f 0)) < (g t) / (2*(f 0)) := by
-  {
-
-    have h_nn : 2*(f 0) ≠ 0 := by {
-      rw [two_mul]
-      simp
-      exact hf_nn 0
-    }
-
-    have h_finite : 2*(f 0) ≠ ∞ := by {
-      rw [two_mul]
-      simp
-      exact hf_finite 0
-    }
-
-    have div_lt : γ / (2*(f 0)) < (g t) / (2*(f 0)) ↔ γ < ((g t) / (2*(f 0))) * (2*(f 0)) := ENNReal.div_lt_iff (Or.inl h_nn) (Or.inl h_finite)
-
-    rw[ENNReal.div_mul_cancel h_nn h_finite] at div_lt
-    rw[div_lt]
-    exact hg t
-  }
-
-  exact gt_of_ge_of_gt f_le div_lt
-
--- Under some non-zero and finite conditions, a =  / (2 * (b / (2 * a))) * b
-lemma ennreal_quo_eq {a b : ℝ≥0∞} (hb1 : b ≠ ∞) (hb2 : b ≠ 0) : a = 1 / (2 * (b / (2 * a))) * b :=
-by
-rw[mul_div 2 b (2 * a), @ENNReal.div_eq_inv_mul (2*b) (2 * a)]
-
-have t : ((2 * a)⁻¹ * (2 * b))⁻¹ = (2 * a) * (2 * b)⁻¹ := by {
-  have temp1 : (2 * b) ≠ 0 := by {
-    simp
-    exact hb2
-  }
-  have temp2 : (2 * b) ≠ ∞ := by {
-    simp
-    exact mul_ne_top (by simp) hb1
-  }
-  rw[ENNReal.mul_inv (Or.inr temp2) (Or.inr temp1)]
-  simp
-}
-rw[one_div, t, ←one_div, mul_one_div]
-have t : 2 * a / (2 * b) = a / b := ENNReal.mul_div_mul_left a b (by simp) (by simp)
-rw[t]
-exact (ENNReal.div_mul_cancel hb2 hb1).symm
-
--- Under some non-zero and finite conditions, a ≤ (1 / (2 * c)) * b → - (b : ℝ) ≤ -2 * (c : ℝ) * (a : ℝ)
-lemma ennreal_quo_ineq (a b c : ℝ≥0∞) (hbI : b ≠ ∞) (hcnn : c ≠ 0) (hcI : c ≠ ∞) (h : a ≤ (1 / (2 * c)) * b) : - ENNReal.toReal b ≤ -2 * ENNReal.toReal c * ENNReal.toReal a := by {
-  have t : 1 / (2 * c) * b = (2 * c)⁻¹ * b := by simp
-  rw [t] at h
-
-  have finite : (2 * c) ≠ ∞ := ENNReal.mul_ne_top (by simp) (hcI)
-  have n_zero : (2 * c) ≠ 0 := mul_ne_zero (by simp) (hcnn)
-  have tt : a * (2 * c) ≤ (2 * c)⁻¹ * b * (2 * c) := by {
-    exact (ENNReal.mul_le_mul_right n_zero finite).mpr h
-  }
-
-  have ttt : (2 * c)⁻¹ * b * (2 * c) = b * ((2 * c)⁻¹ * (2 * c)) := by ring
-  have t : (2 * c)⁻¹ * (2 * c) = 1 := by exact ENNReal.inv_mul_cancel n_zero finite
-  rw [ttt, t, mul_one] at tt
-  have t : ENNReal.toReal (a * (2 * c)) ≤ ENNReal.toReal b := by {
-    exact toReal_mono hbI tt
-  }
-  have tt : ENNReal.toReal (a * (2 * c)) = ENNReal.toReal a * ENNReal.toReal (2 * c) := by simp
-  rw [tt] at t
-  have tt : ENNReal.toReal (2 * c) = ENNReal.toReal 2 * ENNReal.toReal c := by simp
-  rw [tt] at t
-  have tt : ENNReal.toReal a * (ENNReal.toReal 2 * ENNReal.toReal c) = ENNReal.toReal a * ENNReal.toReal 2 * ENNReal.toReal c := by ring
-  rw [tt] at t
-  have tt := neg_le_neg t
-  have t : -(ENNReal.toReal a * ENNReal.toReal 2 * ENNReal.toReal c) = - ENNReal.toReal 2 * ENNReal.toReal c * ENNReal.toReal a := by ring
-  rw [t] at tt
-  exact tt
-}
 
 lemma norm_eq_zero_ (f : ℕ → Vector ℝ d → ℝ) : ‖f‖ = 0 ↔ f = 0 := by sorry
